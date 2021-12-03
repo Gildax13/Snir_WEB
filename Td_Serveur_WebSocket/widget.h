68f3446d83d6129476226aa538ad709fa41d9386
Gildax13 <gildax13.pro@gmail.com>
2021-12-03 09:24:16 +0100

    MAJ exo websocket

--- Td_Serveur_WebSocket/widget.h
+++ Td_Serveur_WebSocket/widget.h
@@ -2,6 +2,9 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QWebSocket>
+#include <QWebSocketServer>
+#include <QTimer>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
@@ -15,7 +18,20 @@ public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
 
+private slots:
+    void onQWebSocketServer_newConnection();
+
+    void onQWebSocket_textMessageReceived(QString);
+
+    void onQtimer_timeOut();
+
+
 private:
     Ui::Widget *ui;
+    QWebSocketServer *sock;
+    QWebSocket *client;
+    QTimer *majDate;
+
+
 };
 #endif // WIDGET_H
